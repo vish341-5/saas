9e0ec59825ef54f4eada29dfeda819c77b58b4cb
vish341-5 <visheshvishnoi799@gmail.com>
2026-05-12 02:58:23 +0530

    commit 3

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
 
 export const supabase = createClient(
-    "https://whqkgfxwijcjecxgkpan.supabase.co",
-    "sb_publishable_4_Ac9qYdWBi2f6YqkneqmQ__VJMSKe4"
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
 )
